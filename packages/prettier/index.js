aa5476928da83ee52fa162b8df8377456847bbb1
Junho Yeo <i@junho.io>
2022-04-28 17:12:00 +0900

    wip

--- packages/prettier/index.js
+++ packages/prettier/index.js
@@ -1,1 +1,14 @@
-module.exports = {};
+module.exports = {
+  semi: true,
+  plugins: [require("@trivago/prettier-plugin-sort-imports")],
+
+  // @trivago/prettier-plugin-sort-imports
+  importOrder: ["<THIRD_PARTY_MODULES>", "@/(.*)$", "^[./](.*)$"],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrderParserPlugins: [
+    "jsx",
+    "typescript",
+    '["decorators", { "decoratorsBeforeExport": true }]',
+  ],
+};
